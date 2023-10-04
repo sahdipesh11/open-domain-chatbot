4a9c88e7de22d49bd8a2e70a07261549c52df4b7
sahd11 <sahdipesh11@gmail.com>
2023-10-04 20:11:44 +1100

    Implement threading so that there is no lagging between input and output.

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ class ChatbotWindow(QMainWindow):
         self.chatbot = Chatbot()
 
         self.setMinimumSize(700, 500)
-        self.setWindowTitle("Chatbot") # Add window title
+        self.setWindowTitle("Chatbot")  # Add window title
 
         # Add chat area widget.
         self.chat_area = QTextEdit(self)
-        self.chat_area.setGeometry(10, 10, 480, 320) # Parameters: x, y, w, h
-        self.chat_area.setReadOnly(True) # Make it read only.
+        self.chat_area.setGeometry(10, 10, 480, 320)  # Parameters: x, y, w, h
+        self.chat_area.setReadOnly(True)  # Make it read only.
 
         # Add input field widget.
         self.input_field = QLineEdit(self)
@@ -32,15 +32,24 @@ class ChatbotWindow(QMainWindow):
 
     def send_message(self):
         user_input = self.input_field.text().strip()
-        self.chat_area.append(f"<p style='color:#333333'>Me: {user_input}</p>") # Append takes string input.
-        self.input_field.clear() # Clear input field.
 
-        # Get response and show it in the chat area with gray background.
+        # Use append method to append the user input in the chat area.
+        self.chat_area.append(
+            f"<p style='color:#333333'>Me: {user_input}</p>")
+        self.input_field.clear()  # Clear input field.
+
+        # Use threading for program efficiency.
+        thread = threading.Thread(target=self.get_bot_response,
+                                  args=(user_input,))
+        thread.start()
+
+    def get_bot_response(self, user_input):
+        # Get response and append it in the chat area with gray background.
         response = self.chatbot.get_response(user_input)
         self.chat_area.append(
             f"<p style='color:#333333; background-color: #E9E9E9'>Bot: {response}</p>")
 
+
 app = QApplication(sys.argv)
 window = ChatbotWindow()
 sys.exit(app.exec())
-
